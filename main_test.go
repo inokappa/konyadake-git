770c914d784710e8680a4a964fa4d2964fec4f2e
inokappa <inokara@gmail.com>
2018-12-30 12:35:21 +0900

    some update

--- main_test.go
+++ main_test.go
@@ -4,7 +4,7 @@ import (
 	_ "bytes"
 	_ "fmt"
 	_ "os/exec"
-	"strings"
+	_ "strings"
 	"testing"
 )
 
@@ -14,30 +14,6 @@ import (
 // 	cmd.Stderr = stdout
 // 	msg := "バージョンを出力."
 // 	_ = cmd.Run()
-// 	if !strings.Contains(stdout.String(), msg) {
-// 		t.Fatal("Failed Test")
-// 	}
-// }
-
-// func TestNoRepoName(t *testing.T) {
-// 	cmd := exec.Command("gom", "run", "main.go")
-// 	stdout := new(bytes.Buffer)
-// 	cmd.Stderr = stdout
-// 	msg := "Git リポジトリを指定して下さい."
-// 	_ = cmd.Run()
-// 	fmt.Println(stdout.String())
-// 	if !strings.Contains(stdout.String(), msg) {
-// 		t.Fatal("Failed Test")
-// 	}
-// }
-//
-// func TestBadRepoName(t *testing.T) {
-// 	cmd := exec.Command("gom", "run", "main.go", "-repo=github.com:inokappa/example.git")
-// 	stdout := new(bytes.Buffer)
-// 	cmd.Stderr = stdout
-// 	msg := "Git リポジトリを指定して下さい."
-// 	_ = cmd.Run()
-// 	fmt.Println(stdout.String())
 // 	if !strings.Contains(stdout.String(), msg) {
 // 		t.Fatal("Failed Test")
 // 	}
